a39223510370c0c719e5ba90c1e470e98cd97a3e
apirrone <awanegenebayo@gmail.com>
2025-01-13 10:22:45 +0100

    update

--- scripts/goto_init_position.py
+++ scripts/goto_init_position.py
@@ -1,6 +1,20 @@
 from pypot.feetech import FeetechSTS3215IO
-from mini_bdx_runtime.hwi_feetech_pypot import HWI
+# from mini_bdx_runtime.hwi_feetech_pypot import HWI
 import time
+
+
+io = FeetechSTS3215IO(
+    "/dev/ttyACM0",
+    baudrate=1000000,
+    use_sync_read=True,
+)
+id = 24
+
+io.enable_torque([id])
+io.set_mode({id: 0})
+io.set_goal_position({id: 0})
+time.sleep(1)
+exit()
 
 hwi = HWI()
 # hwi.turn_off()
