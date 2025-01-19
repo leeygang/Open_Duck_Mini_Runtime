0761de177ef4a6115241d701b0d538f9be168d7d
apirrone <awanegenebayo@gmail.com>
2025-01-19 19:25:03 +0100

    check voltage script

---
+++ scripts/check_voltage.py
@@ -0,0 +1,11 @@
+from pypot.feetech import FeetechSTS3215IO
+
+io = FeetechSTS3215IO(
+    "/dev/ttyACM0",
+    baudrate=1000000,
+    use_sync_read=True,
+)
+
+id = 20
+
+print(io.get_present_voltage(id))
