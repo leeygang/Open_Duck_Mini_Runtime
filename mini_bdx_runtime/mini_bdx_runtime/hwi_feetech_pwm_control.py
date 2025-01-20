394631978e84e6f8f6aaead7e42f00a668ab5c57
apirrone <awanegenebayo@gmail.com>
2025-01-20 14:12:07 +0100

    test

--- mini_bdx_runtime/mini_bdx_runtime/hwi_feetech_pwm_control.py
+++ mini_bdx_runtime/mini_bdx_runtime/hwi_feetech_pwm_control.py
@@ -104,7 +104,7 @@ class HWI:
 
     def turn_on(self):
         self.control.set_kps(self.low_torque_kps)
-        # self.control.enable_torque()
+        self.control.enable_torque()
         print("turn on : low KPS set")
         time.sleep(1)
 
