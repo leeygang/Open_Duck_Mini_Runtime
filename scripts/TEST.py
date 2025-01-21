a83a44313aac3aa923d9384ebf64cc6171227895
apirrone <awanegenebayo@gmail.com>
2025-01-21 15:26:57 +0100

    TEST

--- scripts/TEST.py
+++ scripts/TEST.py
@@ -17,7 +17,7 @@ while True:
     # target = 0.4
     target = 0.2 * np.around(0.2*np.sin(2*np.pi*0.1*time.time()), 3)
     zero_pos[name] = target
-    # hwi.set_position_all(zero_pos)
+    hwi.set_position_all(zero_pos)
     present_positions = hwi.get_present_positions()
     present_velocities = hwi.get_present_velocities()
     # print(f"target : {target}, pos : {present_positions[id]}, diff : {target - present_positions[id]}")
