2c5f9bf4c25cd1c4a11d23de7d9d6dcea1a3d4bf
apirrone <awanegenebayo@gmail.com>
2025-01-21 15:23:39 +0100

    TEST

--- scripts/TEST.py
+++ scripts/TEST.py
@@ -5,20 +5,20 @@ import time
 
 hwi = HWI()
 hwi.turn_on()
-while True:
-    time.sleep(0.01)
+# while True:
+#     time.sleep(0.01)
 # exit()
 
-# zero_pos = hwi.zero_pos.copy()
-# id = 4
-# name = list(hwi.joints.keys())[id]
-# while True:
-#     # target = 0.4
-#     target = np.around(0.2*np.sin(2*np.pi*0.1*time.time()), 3)
-#     zero_pos[name] = target
-#     hwi.set_position_all(zero_pos)
-#     present_positions = hwi.get_present_positions()
-#     present_velocities = hwi.get_present_velocities()
-#     # print(f"target : {target}, pos : {present_positions[id]}, diff : {target - present_positions[id]}")
-#     print(f"vel : {present_velocities[id]}")
-#     # # hwi.set_position("left_hip_yaw", 0.2)
+zero_pos = hwi.init_pos.copy()
+id = 4
+name = list(hwi.joints.keys())[id]
+while True:
+    # target = 0.4
+    target = 0.2 * np.around(0.2*np.sin(2*np.pi*0.1*time.time()), 3)
+    zero_pos[name] = target
+    hwi.set_position_all(zero_pos)
+    present_positions = hwi.get_present_positions()
+    present_velocities = hwi.get_present_velocities()
+    # print(f"target : {target}, pos : {present_positions[id]}, diff : {target - present_positions[id]}")
+    print(f"vel : {present_velocities[id]}")
+    # # hwi.set_position("left_hip_yaw", 0.2)
