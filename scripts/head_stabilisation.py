72a2781285fc5d5595d778bf40dabecb30e8dcdf
apirrone <awanegenebayo@gmail.com>
2025-01-24 23:57:41 +0100

    head stabilization test

--- scripts/head_stabilisation.py
+++ scripts/head_stabilisation.py
@@ -8,7 +8,7 @@ imu = Imu(control_freq)
 
 hwi = HWI("/dev/ttyACM0")
 hwi.turn_on()
-kps = [0, 0, 0, 0, 0, 16, 16, 16, 16, 0, 0, 0, 0, 0]
+kps = [0, 0, 0, 0, 0, 32, 32, 32, 32, 0, 0, 0, 0, 0]
 hwi.set_kps(kps)
 
 init_pos = hwi.init_pos
