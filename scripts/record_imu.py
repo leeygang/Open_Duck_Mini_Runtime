fb026ffebe62bf87a9e1e20f352f7fcea325ec2f
apirrone <awanegenebayo@gmail.com>
2025-01-26 10:44:32 +0100

    imu data

--- scripts/record_imu.py
+++ scripts/record_imu.py
@@ -8,6 +8,7 @@ uart = serial.Serial("/dev/ttyS0")  # , baudrate=115200)
 imu = adafruit_bno055.BNO055_UART(uart)
 imu.mode = adafruit_bno055.IMUPLUS_MODE
 data = []
+data_euler = []
 print("Starting to record. Press Ctrl+C to stop.")
 try: 
     while True:
@@ -15,6 +16,7 @@ try:
             raw_orientation = imu.quaternion  # quat
             data.append(raw_orientation)
             euler = R.from_quat(raw_orientation).as_euler("xyz")
+            data_euler.append(euler)
             print(euler)
         except Exception as e:
             print(e)
@@ -25,6 +27,8 @@ except KeyboardInterrupt:
     print("Saving data...")
     with open("imu_data.pkl", "wb") as f:
         pickle.dump(data, f)
+    with open("imu_data_euler.pkl", "wb") as f:
+        pickle.dump(data_euler, f)
     print("Data saved.")
     exit(0)
 
