3954b648afdc4b2d5df812b21a9f6012e33ffecb
apirrone <awanegenebayo@gmail.com>
2025-01-26 11:31:02 +0100

    i2c imu test

---
+++ scripts/i2c_imu_test.py
@@ -0,0 +1,12 @@
+import board
+import busio
+import time
+import adafruit_bno055
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+while True:
+    s = time.time()
+    quat = sensor.quaternion
+    print("getting quat took : ", round(time.time()-s, 3))
+    time.sleep(0.01)
