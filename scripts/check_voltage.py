4f54933ca6c0dae6582d56ec4b6e29b6b665ce1d
apirrone <awanegenebayo@gmail.com>
2025-01-28 17:42:43 +0100

    check voltages

--- scripts/check_voltage.py
+++ scripts/check_voltage.py
@@ -6,6 +6,26 @@ io = FeetechSTS3215IO(
     use_sync_read=True,
 )
 
-id = 20
+joints = {
+    "left_hip_yaw": 20,
+    "left_hip_roll": 21,
+    "left_hip_pitch": 22,
+    "left_knee": 23,
+    "left_ankle": 24,
+    "neck_pitch": 30,
+    "head_pitch": 31,
+    "head_yaw": 32,
+    "head_roll": 33,
+    # "left_antenna": None,
+    # "right_antenna": None,
+    "right_hip_yaw": 10,
+    "right_hip_roll": 11,
+    "right_hip_pitch": 12,
+    "right_knee": 13,
+    "right_ankle": 14,
+}
 
-print(io.get_present_voltage([id]))
+
+voltages = io.get_present_voltage(list(joints.values()))
+for i, name in enumerate(joints.keys()):
+    print(name, round(voltages[i] * 0.1, 2), "V")
