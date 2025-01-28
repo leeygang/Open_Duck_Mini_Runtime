4f736947831e5186e76bce10ffabdb24e460a999
apirrone <awanegenebayo@gmail.com>
2025-01-28 18:42:55 +0100

    imu client server

--- scripts/imu_client.py
+++ scripts/imu_client.py
@@ -1,15 +1,19 @@
 import socket
 import time
+import numpy as np
 import pickle
 from queue import Queue
 from threading import Thread
+from scipy.spatial.transform import Rotation as R
+from FramesViewer.viewer import Viewer
+import argparse
 
 
 class IMUClient:
-    def __init__(self, host, port=1234, freq=10):
+    def __init__(self, host, port=1234, freq=30):
         self.host = host
         self.port = port
-        self.freq = 30
+        self.freq = freq
         self.client_socket = socket.socket()
         self.client_socket.connect((self.host, self.port))
         self.imu_queue = Queue(maxsize=1)
@@ -39,8 +43,26 @@ class IMUClient:
 
 
 if __name__ == "__main__":
-    client = IMUClient("192.168.89.246")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pitch_bias", type=float, default=0, help="deg")
+    args = parser.parse_args()
+
+    client = IMUClient("192.168.166.253")
+    fv = Viewer()
+    fv.start()
+    pose = np.eye(4)
+    pose[:3, 3] = [0.1, 0.1, 0.1]
 
     while True:
-        print(client.get_imu())
+        quat = client.get_imu()
+        try:
+            euler = R.from_quat(quat).as_euler("xyz")
+            euler[1] += np.deg2rad(args.pitch_bias)
+            rot_mat = R.from_euler("xyz", euler).as_matrix()
+            # rot_mat = R.from_quat(quat).as_matrix()
+            # print(euler)
+            pose[:3, :3] = rot_mat
+            fv.pushFrame(pose, "aze")
+        except:
+            pass
         time.sleep(1 / 30)
