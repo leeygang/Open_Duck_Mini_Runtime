6174100ca68c8866b3d39e90fc14efb2f76cbf3a
apirrone <awanegenebayo@gmail.com>
2025-01-29 18:53:57 +0100

    update

--- scripts/v2_rl_walk.py
+++ scripts/v2_rl_walk.py
@@ -122,8 +122,8 @@ class RLWalk:
         return np.array(pos_with_antennas)
 
     def set_zero_head(self, pos):
-        pos[5] = np.deg2rad(30)
-        pos[6] = np.deg2rad(-30)
+        pos[5] = np.deg2rad(20)
+        pos[6] = np.deg2rad(-20)
         # pos[5] = 0
         # pos[6] = 0
         pos[7] = 0
