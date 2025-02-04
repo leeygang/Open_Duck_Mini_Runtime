207f08891b97211b116587e0d4f162c5a6e09404
apirrone <awanegenebayo@gmail.com>
2025-02-04 18:07:11 +0100

    move all motors

--- scripts/move_all_motors.py
+++ scripts/move_all_motors.py
@@ -6,6 +6,10 @@ import RPi.GPIO as GPIO
 LEFT_FOOT_PIN = 22
 RIGHT_FOOT_PIN = 27
 
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
+GPIO.setup(LEFT_FOOT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RIGHT_FOOT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 hwi = HWI()
 hwi.turn_on()
 
