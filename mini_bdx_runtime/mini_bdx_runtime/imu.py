e4f1a63c35dd15edad05b158540eb3e3369449db
apirrone <awanegenebayo@gmail.com>
2025-02-04 18:44:20 +0100

    imu

--- mini_bdx_runtime/mini_bdx_runtime/imu.py
+++ mini_bdx_runtime/mini_bdx_runtime/imu.py
@@ -97,6 +97,7 @@ class Imu:
             s = time.time()
             try:
                 raw_orientation = np.array(self.imu.quaternion)  # quat
+                print(raw_orientation)
                 euler = R.from_quat(raw_orientation).as_euler("xyz")
             except Exception as e:
                 print("[IMU]:", e)
