6f64e70f4e474af2cee3fc60f521b0d05ac9eb43
apirrone <awanegenebayo@gmail.com>
2025-02-05 13:45:51 +0100

    imu

--- mini_bdx_runtime/mini_bdx_runtime/imu.py
+++ mini_bdx_runtime/mini_bdx_runtime/imu.py
@@ -49,11 +49,11 @@ class Imu:
                 exit()
 
         # # calibrate imu
-        # calibrated = self.imu.calibration_status[1] == 3
-        # print("Calibrating Gyro...")
-        # while not calibrated:
-        #     calibrated = self.imu.calibration_status[1] == 3
-        #     time.sleep(0.01)
+        calibrated = self.imu.calibration_status[1] == 3
+        print("Calibrating Gyro...")
+        while not calibrated:
+            calibrated = self.imu.calibration_status[1] == 3
+            time.sleep(0.01)
 
         # self.zero_euler = None
         # while self.zero_euler is None:
