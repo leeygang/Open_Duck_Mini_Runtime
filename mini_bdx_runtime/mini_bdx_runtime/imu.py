b54672570dd7d223f7624624b489c9c61a1a8731
apirrone <awanegenebayo@gmail.com>
2025-02-05 13:05:28 +0100

    bump scipy version

--- mini_bdx_runtime/mini_bdx_runtime/imu.py
+++ mini_bdx_runtime/mini_bdx_runtime/imu.py
@@ -1,14 +1,13 @@
 import adafruit_bno055
-import board
-import busio
+# import board
+# import busio
 import numpy as np
+import serial
 
 from queue import Queue
 from threading import Thread
 import time
 from scipy.spatial.transform import Rotation as R
-
-
 
 # TODO filter spikes
 class Imu:
@@ -17,8 +16,11 @@ class Imu:
         self.user_pitch_bias = user_pitch_bias
         self.nominal_pitch_bias = 20
 
-        i2c = busio.I2C(board.SCL, board.SDA)
-        self.imu = adafruit_bno055.BNO055_I2C(i2c)
+        self.uart = serial.Serial("/dev/ttyS0", baudrate=115200)
+        self.imu = adafruit_bno055.BNO055_UART(self.uart)
+
+        # i2c = busio.I2C(board.SCL, board.SDA)
+        # self.imu = adafruit_bno055.BNO055_I2C(i2c)
 
         self.imu.mode = adafruit_bno055.IMUPLUS_MODE
         # self.imu.mode = adafruit_bno055.ACCGYRO_MODE
