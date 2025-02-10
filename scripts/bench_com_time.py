c6cac34bde4a21c5919941d009f593dd9a67c548
apirrone <awanegenebayo@gmail.com>
2025-02-10 11:29:26 +0100

    bench_com_time

--- scripts/bench_com_time.py
+++ scripts/bench_com_time.py
@@ -5,12 +5,12 @@ import numpy as np
 io = FeetechSTS3215IO(
     "/dev/ttyACM0",
     baudrate=1000000,
-    # use_sync_read=False,
-    use_sync_read=True,
+    use_sync_read=False,
+    # use_sync_read=True,
 )
 
-ids = [1]
-# ids = [10, 11, 12, 13, 14, 20, 21, 22, 23, 24, 30, 31, 32, 33]
+# ids = [1]
+ids = [10, 11, 12, 13, 14, 20, 21, 22, 23, 24, 30, 31, 32, 33]
 
 # io.enable_torque(ids)
 # io.set_mode({id: 0 for id in ids})
