dc3b6af8a080ae1e7ed9f2863f98b36a6441a570
apirrone <awanegenebayo@gmail.com>
2025-02-25 11:37:32 +0100

    turn off, turn on update

--- scripts/turn_on.py
+++ scripts/turn_on.py
@@ -1,4 +1,5 @@
-from mini_bdx_runtime.hwi_feetech_pypot import HWI
+# from mini_bdx_runtime.hwi_feetech_pypot import HWI
+from mini_bdx_runtime.rustypot_control_hwi import HWI
 
 hwi = HWI()
 hwi.turn_on()
