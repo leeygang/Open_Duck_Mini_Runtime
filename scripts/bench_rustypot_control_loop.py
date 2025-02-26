c0fe568f97cb44ddd1d6a63ccdf5cc850670e427
apirrone <awanegenebayo@gmail.com>
2025-02-26 11:13:46 +0100

    bench rustypot control loop

---
+++ scripts/bench_rustypot_control_loop.py
@@ -0,0 +1,51 @@
+from mini_bdx_runtime.hwi_feetech_pwm_control import HWI
+import time
+import numpy as np
+
+joints_order = [
+    "left_hip_yaw",
+    "left_hip_roll",
+    "left_hip_pitch",
+    "left_knee",
+    "left_ankle",
+    "neck_pitch",
+    "head_pitch",
+    "head_yaw",
+    "head_roll",
+    "left_antenna",
+    "right_antenna",
+    "right_hip_yaw",
+    "right_hip_roll",
+    "right_hip_pitch",
+    "right_knee",
+    "right_ankle",
+]
+
+
+def make_action_dict(action, joints_order):
+    action_dict = {}
+    for i, a in enumerate(action):
+        if "antenna" not in joints_order[i]:
+            action_dict[joints_order[i]] = a
+
+    return action_dict
+
+hwi = HWI("/dev/ttyACM0")
+hwi.turn_on()
+
+time.sleep(1)
+
+starting_pos_rad = np.array(hwi.get_present_positions())
+freq = 50
+A = np.deg2rad(10)
+F = 0.1
+while True:
+
+    action = starting_pos_rad + A * np.sin(2*np.pi*F*time.time())
+    action_dict = make_action_dict(action, joints_order)
+    hwi.set_position_all(action_dict)
+    
+    time.sleep(1/freq)
+
+
+
