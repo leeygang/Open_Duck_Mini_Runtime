6faec97dcd9e62f498ba51b8d5485a43f5d274b7
apirrone <awanegenebayo@gmail.com>
2025-04-07 11:33:24 +0200

    head mix

--- scripts/v2_rl_walk_mujoco.py
+++ scripts/v2_rl_walk_mujoco.py
@@ -289,8 +289,9 @@ class RLWalk:
                     ):  # give time to the filter to stabilize
                         self.motor_targets = filtered_motor_targets
 
+                head_motor_targets = self.last_commands[3:] + self.motor_targets[5:9]
+                self.motor_targets[5:9] = head_motor_targets
                 self.prev_motor_targets = self.motor_targets.copy()
-                # self.motor_targets[5:9] = self.last_commands[3:]
 
                 action_dict = make_action_dict(self.motor_targets, joints_order)
 
